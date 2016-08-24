e094e38246e896478304a53dc7e275f3b263ca70
alexandergrg <alexandergrg@hotmail.com>
2016-08-24 16:19:15 -0500

    Avance etapa 3.10 elaboraciòn de la arquitectura de MVC, carpeta HTML, y agragaciòn del animate.css

---
+++ core/bd/database.sql
@@ -0,0 +1,16 @@
+
+-- CREAR LA BASE DE DATOS CON EL COTEJAMIENTO
+
+-- CREATE DATABASE `sigcombdb` DEFAULT CHARSET=utf8 COLLATE utf8_unicode_ci;
+
+
+CREATE TABLE `sigcombdb`.`users` (
+  `id` INT(255) NOT NULL AUTO_INCREMENT ,
+  `user` VARCHAR(50) NOT NULL ,
+  `pass` VARCHAR(50) NOT NULL ,
+  `email` VARCHAR(50) NOT NULL ,
+  `permisos` INT(1) NOT NULL DEFAULT '0' ,
+  PRIMARY KEY (`id`)) ENGINE = InnoDB CHARSET=utf8 COLLATE utf8_unicode_ci;
+
+  -- INGRESO DE USUARIO DE ADMINISTRADOR PARA PRUEBAS DE SISTEMA
+  INSERT INTO `users` (`id`, `user`, `pass`, `email`, `permisos`) VALUES (NULL, 'alexandergrg', '123456', 'alexandergrg@hotmail.com', '2');
--- html/index/index.php
+++ html/index/index.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>SIGCOMB | Login</title>
+    <title>SIGCOMB | Ingreso</title>
 
     <!-- Bootstrap -->
     <link href="views/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -29,7 +29,7 @@
         <div class="animate form login_form">
           <section class="login_content">
             <form>
-              <h1>Login Form</h1>
+              <h1>Login </h1>
               <div>
                 <input type="text" class="form-control" placeholder="Username" required="" />
               </div>
@@ -37,15 +37,15 @@
                 <input type="password" class="form-control" placeholder="Password" required="" />
               </div>
               <div>
-                <a class="btn btn-default submit" href="index.html">Log in</a>
-                <a class="reset_pass" href="#">Lost your password?</a>
+                <a class="btn btn-primary submit" href="index.html">Log in</a>
+                <a class="reset_pass" href="#">¿Olvidaste tu Contraseña?</a>
               </div>
 
               <div class="clearfix"></div>
 
               <div class="separator">
-                <p class="change_link">New to site?
-                  <a href="#signup" class="to_register"> Create Account </a>
+                <p class="change_link">Si deseas crear un usuario |
+                  <a href="#signup" class="to_register"> Registrate </a>
                 </p>
 
                 <div class="clearfix"></div>
@@ -63,7 +63,7 @@
         <div id="register" class="animate form registration_form">
           <section class="login_content">
             <form>
-              <h1>Create Account</h1>
+              <h1>Registrate</h1>
               <div>
                 <input type="text" class="form-control" placeholder="Username" required="" />
               </div>
@@ -74,14 +74,14 @@
                 <input type="password" class="form-control" placeholder="Password" required="" />
               </div>
               <div>
-                <a class="btn btn-default submit" href="index.html">Submit</a>
+                <a class="btn btn-primary submit" href="index.html">Enviar</a>
               </div>
 
               <div class="clearfix"></div>
 
               <div class="separator">
-                <p class="change_link">Already a member ?
-                  <a href="#signin" class="to_register"> Log in </a>
+                <p class="change_link">Si ya estás listo |
+                  <a href="#signin" class="to_register"> Ingresar </a>
                 </p>
 
                 <div class="clearfix"></div>
