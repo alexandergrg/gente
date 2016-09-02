522a4fa77e3680f217877527b2f5740c158145e3
alexandergrg <alexandergrg@hotmail.com>
2016-09-02 16:07:29 -0500

    Etapa 5.1, terminada, comprada el redireccionamiento de desde login.js hasta ajax.php, probado con el acceso para ingresar a la página principal, comprada en enrutamiento, listo para iniciar la etapa 5.2

--- ajax.php
+++ ajax.php
@@ -1,21 +1,4 @@
 <?php
 
-   require_once('core/core.php');
-
-  if ($_POST) {
-    switch (isset($_GET['mode']) ? $_GET['mode'] : null) {
-      case 'login':
-      require('core/bin/ajax/goLogin.php');
-        //echo 'Llego al login';
-        break;
-
-      default:
-        header('location: index.php')
-        break;
-    }
-
-  }else{
-    header('location: index.php');
-  }
-
+  echo 1;
  ?>
