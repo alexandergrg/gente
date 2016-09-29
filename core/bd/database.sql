454579903fb6f111e1b7ffea1d5340ecc59191f5
alexandergrg <alexandergrg@hotmail.com>
2016-09-29 16:12:43 -0500

    MODIFICACIÓN DEL ARCHIVO DE LA BASE DE DATOS
    	1.- Creación modificación de la base de datos con nuevas tablas y con usuario de inicio de sesion
    	2.- Creación de las Tabla Perfiles, Usuarios y Funcionarios.
    	3.- Inserción de registros en la tabla Perfiles para hacer la prueba.
    	4.- Instalación de Navicat Administrador de base de datos.

--- core/bd/database.sql
+++ core/bd/database.sql
@@ -1,15 +1,59 @@
 
 -- CREAR LA BASE DE DATOS CON EL COTEJAMIENTO
 
--- CREATE DATABASE `sigcombdb` DEFAULT CHARSET=utf8 COLLATE utf8_unicode_ci;
+-- CREATE DATABASE `sigcombdb` DEFAULT CHARSET=utf8 COLLATE utf8_spanish_ci;
+
+-- GRANT ALL PRIVILEGES ON sigcombdb.* TO is_sigcombdb WITH GRANT OPTION;
+
+--  CREACION DE LA TABLA PERFILES
+
+CREATE TABLE perfiles(
+	id INT (255) NOT NULL AUTO_INCREMENT ,
+  nombre VARCHAR(50) NOT NULL,
+  permiso INT(1),
+  estado INT(1) NOT NULL DEFAULT 1,
+  CONSTRAINT pk_perfiles PRIMARY KEY (id),
+  CONSTRAINT uq_nombre UNIQUE (nombre)
+);
+
+--  CREACION DE LA TABLA USUARIOS
+
+CREATE TABLE usuarios (
+	id INT(255) NOT NULL AUTO_INCREMENT ,
+  usuario VARCHAR(50) NOT NULL ,
+  pass VARCHAR(100) NOT NULL ,
+  correo VARCHAR(50) NOT NULL ,
+	estado INT(1) NOT  NULL DEFAULT 1,
+	idPerfiles INT(255) NOT NULL,
+  CONSTRAINT pk_usuarios  PRIMARY KEY (id),
+	CONSTRAINT uq_usuario UNIQUE(usuario),
+	CONSTRAINT fk_usuarios_perfiles FOREIGN KEY(idPerfiles) REFERENCES perfiles (id)
+															ON UPDATE RESTRICT ON DELETE RESTRICT
+);
 
 
-CREATE TABLE `sigcombdb`.`users` (
-  `id` INT(255) NOT NULL AUTO_INCREMENT ,
-  `user` VARCHAR(50) NOT NULL ,
-  `pass` VARCHAR(50) NOT NULL ,
-  `email` VARCHAR(50) NOT NULL ,
-  `permisos` INT(1) NOT NULL DEFAULT '0' ,
-  PRIMARY KEY (`id`)) ENGINE = InnoDB CHARSET=utf8 COLLATE utf8_unicode_ci;
+--  CREACION DE LA TABLA FUNCIONARIOS
 
-  
+ CREATE TABLE funcionarios(
+	id INT (255) NOT NULL AUTO_INCREMENT,
+	cedula VARCHAR(255) NOT NULL,
+	nombres VARCHAR(100) NOT NULL,
+	apellidos VARCHAR(100) NOT NULL,
+	correo VARCHAR(255) NOT NULL,
+	fechanacimiento DATETIME DEFAULT NOW(),
+	estadocivil VARCHAR(20),
+	genero VARCHAR(10),
+	estado INT(1) NOT NULL DEFAULT 1,
+	idusuario INT(255) NULL,
+	CONSTRAINT pk_funcionarios PRIMARY KEY (id),
+	CONSTRAINT uq_cedula UNIQUE(cedula),
+	CONSTRAINT uq_correo UNIQUE(correo),
+	CONSTRAINT fk_funcionarios_usuarios FOREIGN KEY (idusuario) REFERENCES usuarios (id)
+															ON UPDATE RESTRICT ON DELETE RESTRICT
+);
+
+-- AGREGACION DE CAMPO IDFUNCIONARIOS EN LA TABLA USUARIOS
+ALTER TABLE usuarios
+ADD COLUMN idfuncionarios  int(255) NULL AFTER idPerfiles;
+
+ALTER TABLE usuarios ADD CONSTRAINT fk_usuarios_funcionarios FOREIGN KEY (idfuncionarios) REFERENCES funcionarios (id) ON DELETE RESTRICT ON UPDATE RESTRICT;
